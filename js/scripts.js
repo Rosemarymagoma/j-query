a95f049e1120c262e78fa39e69aedc53434eee7a
rosemarymagoma99@gmail.com <rosemarymagoma99@gmail.com>
2018-09-19 04:34:47 +0300

    Add methods to paragraphs

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
+
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
+    $(".clickable").click(function() {
+    $("#walrus-showing").slideDown();
+    $("#walrus-hidden").slideToggle();
   });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-  });
+});
